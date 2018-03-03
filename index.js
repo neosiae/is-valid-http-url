ace8209daaf8a94d11b5d209b4dec356ccc337ba
Denis Kotlica <7550223+neosiae@users.noreply.github.com>
2018-03-03 19:08:53 +0100

    Change IPv4 regex

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ module.exports = (url) => {
       URL type is invalid. Expected a string, but got: ${typeOfUrl}
     `);
   }
-
-  const protocol = '(?:(?:https?)://)?';
-  const ipv4 = '(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)';
+  
+  const protocol = '(?:(?:https?)://)';
+  const ipv4 = '(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))';
   const hostname = '(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)';
   const domain = '(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*';
   const tld = '(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))';
